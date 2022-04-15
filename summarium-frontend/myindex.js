20e86e9d8e3f29ad53f7eee19c0fcfc8cbf2e1c3
KapadiaNaitik <88614335+KapadiaNaitik@users.noreply.github.com>
2022-04-15 20:37:10 +0530

    Adding Code For  Frontend  Part

---
+++ summarium-frontend/myindex.js
@@ -0,0 +1,7 @@
+function handleFiles(event) {
+    var files = event.target.files;
+    $("#src").attr("src", URL.createObjectURL(files[0]));
+    document.getElementById("audio").load();
+}
+
+document.getElementById("upload").addEventListener("change", handleFiles, false);
